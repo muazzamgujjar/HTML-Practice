707dde89ef1937cf95a07a63ba222bfe51c44596
shazib <muazzamgujjarmuazzam@gmail.com>
2023-07-12 04:58:23 -0700

    first comment

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("IM a programmer")
+// objcts
+//  Object creat in java script by let,var,const,
+
+ let 
+ person = {
+    name: shazib,
+    age: 20,
+    education: "undergeratuation"
+ }
+  console.log(person)
