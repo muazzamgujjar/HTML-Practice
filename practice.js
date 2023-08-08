1e32e1d2e09dbe2226e31160bb5771d0645d886c
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-08-07 23:10:56 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,15 @@
+//  Q#1
+// // let a = "alistring"
+// // let b = 8
+// // console.log(a+b)
+
+// // // Q #2
+// // console.log(typeof (a+b))
+
+// // Q # 3
+ var  a1 ={
+    name: "Gujjar",
+    age:23,    
+}
+
+console.log(a1)
--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // objcts
 //  Object creat in java script by let,var,const,
 
- let 
- person = {
-    name: shazib,
-    age: 20,
-    education: "undergeratuation"
- }
-  console.log(person)
+//  let 
+//  person = {
+//     name: "umer",
+
+
+//     age: "20",
+//     education: "undergeratuation"
+//  }
+//   console.log(person)
+  var num2 = 9;
+  var num2 = 2;
+  console.log(num2)
+  num2 = 9;
