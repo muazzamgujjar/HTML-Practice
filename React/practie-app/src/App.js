cc0ca9c2986a319f3406422fe5022867ef78cce0
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-08-23 07:06:01 -0700

    react app

---
+++ React/practie-app/src/App.js
@@ -0,0 +1,14 @@
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import './App.scss'; 
+import Header from "./Components/Common/Header";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  );
+}
+export default  App;
